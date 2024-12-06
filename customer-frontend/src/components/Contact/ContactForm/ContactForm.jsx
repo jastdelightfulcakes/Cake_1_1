95dec694a0b6fc621ce68d6ea7a421f2d2088208
jastdelightfulcakes <jastdelightfulcake@gmail.com>
2024-12-06 09:49:48 -0700

    Update ContactForm.jsx

--- customer-frontend/src/components/Contact/ContactForm/ContactForm.jsx
+++ customer-frontend/src/components/Contact/ContactForm/ContactForm.jsx
@@ -26,6 +26,7 @@ export const ContactFrom = () => {
                         <p>
                             Write to us if you have any questions, we will definitely contact
                             you and find a solution.
+                            <br></br>
                             If you want to place an order, then mention entire order details including date and other requirements.
                             <br></br>
                             (NOTE: Do not forget to mention your phone number.)
